141c17fa96e2627eb1a8bfb99361fb27dd258780
User 12 <User 12@PC17>
2021-10-27 14:48:35 +0300

    Add project files.

---
+++ na obratno/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace na_obratno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] cArray = text.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            Console.WriteLine(reverse); 
+        }
+    }
+}
